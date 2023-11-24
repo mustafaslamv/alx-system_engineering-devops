0c13b097e0dfd0252c2c8a88b900da33e842d72c
mustafa slama <ayman1slama@gmail.com>
2023-11-24 21:27:25 +0200

    create file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creating file in /tmp using pupper
+
+file { '/tmp/school':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
